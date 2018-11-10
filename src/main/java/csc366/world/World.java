b9d0ac594ffffcf263daa0d58a59624c4e1a5623
iramazan <iramazan96@gmail.com>
2018-11-10 18:48:19 -0500

    Establish behind and beside relationships.

--- src/main/java/csc366/world/World.java
+++ src/main/java/csc366/world/World.java
@@ -20,15 +20,39 @@ public class World {
         on(blueBlock, greenBlock);
         on(redPyramid, table);
         on(bluePyramid, table);
+        beside(redBlock, bluePyramid);
+        beside(bluePyramid, greenBlock);
+        behind(redBlock, bluePyramid);
+        behind(redBlock, greenBlock);
     }
 
     /**
-     * Establish the relationship shape on support
+     * Establish the relationship: shape on support.
      * @param shape Shape which will be on top of some support
      * @param support support which will be supporting the object
      */
     public void on(Shape shape, Supporter support) {
         shape.setSupportedBy(support);
         support.setSupporting(shape);
+    }
+
+    /**
+     * Establish the relationship: One shape is beside another.
+     * @param left The left shape
+     * @param right The right shape
+     */
+    public void beside(Shape left, Shape right) {
+        left.setRight(right);
+        right.setLeft(left);
+    }
+
+    /**
+     * Establish the relationship: One shape is behind another.
+     * @param back The shape which is farther to the back
+     * @param front The shape which is farther to the front
+     */
+    public void behind(Shape back, Shape front) {
+        back.setInFront(front);
+        front.setBehind(back);
     }
 }
